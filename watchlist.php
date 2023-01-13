eb9e6bac86344b38254c28069c9258d6ce5ac37b
Jorn <jorn.kops@student.uva.nl>
2023-01-13 11:48:30 +0100

    git setup

---
+++ watchlist.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Watchlist</title>
+        <link rel="stylesheet" href="watchlist.css">
+    </head>
+    <body>
+        <div class="form">
+            <form action="" class="search-bar">
+                <input type="text" placeholder="search a title" name="name">
+                <button type="submit"></button>
+            </form>
+        </div>
+
+        <div class="tab">
+            <button class="tablinks" onclick="openWatch(event, 'Future Watching')">Future Watching</button>
+            <button class="tablinks" onclick="openWatch(event, 'Currently Watching')" id="defaultOpen">Currently Watching</button>
+            <button class="tablinks" onclick="openWatch(event, 'Finished Watching')">Finished Watching</button>
+        </div>
+
+        <div id="Future Watching" class="tabcontent">
+            <div class="moviesection">
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>The Godfather</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="Currently Watching" class="tabcontent">
+            <div class="moviesection">
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>Avatar</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="Finished Watching" class="tabcontent">      
+            <div class="moviesection">
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/M/MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_QL75_UX190_CR0,2,190,281_.jpg">
+                    <div class="content">
+                        <div class="row">
+                            <div class="details">
+                                <h3>The Menu</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <script src="watchlist.js"></script>
+    </body>
+</html> 
