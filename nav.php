c1833704a805f162dd56ce2b38c1efaa0ea02143
marco <marco.blok@student.uva.nl>
2023-01-15 15:45:58 +0100

    made navbar modular + some css tweaks

---
+++ nav.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<div class="navbar" id="navdiv">
+    <nav>
+        <ul>
+            <li><a href="">HOME</a></li>
+            <li><a href="">REGION</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">LOGIN</a></li>
+        </ul>
+    </nav>
+</div>';
+
+?>
