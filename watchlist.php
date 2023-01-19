4b52b36ec818b5f808c68658706803133a47daed
danielo <daniel.oppenhuizen@student.uva.nl>
2023-01-19 19:03:33 +0100

    added complete watchlist template

--- watchlist.php
+++ watchlist.php
@@ -13,75 +13,86 @@
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     </head>
     <body>
-    
+
         <!-- insert the nav bar -->
         <?php require_once("nav.php")?>
 
-        <div class="tab">
-            <button class="tablinks" onclick="openWatch(event, 'Future Watching')" id="defaultOpen">Future Watching</button>
-            <button class="tablinks" onclick="openWatch(event, 'Currently Watching')">Currently Watching</button>
-            <button class="tablinks" onclick="openWatch(event, 'Finished Watching')">Finished Watching</button>
-        </div>
+        <div class="content" id="contentID" style="background: rgb(108, 132, 140);">
+            
+            <div class="searchrow" id="formID">
+                <form class="search-bar" name="search" action="#" onsubmit="searchbutton();return false">
+                    <input type="text" autocomplete="off" placeholder="search a title" name="name" id="textbar">
+                    <button type="submit" id="buttonID"></button>
+                </form>
+            </div>
 
-        <div id="Future Watching" class="tabcontent">
-            <div class="moviesection">
-                <div class="card">
-                    <div class="imagebox">
-                        <img src="https://image.tmdb.org/t/p/original/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg" class="poster">
-                        <div class="streamingservice">
-                            <img src="streaming_img/prime.png">
+            <div class="tabrow">
+                <div class="tab">
+                    <button class="tablinks" onclick="openWatch(event, 'Future Watching')" id="defaultOpen">Future Watching</button>
+                    <button class="tablinks" onclick="openWatch(event, 'Currently Watching')">Currently Watching</button>
+                    <button class="tablinks" onclick="openWatch(event, 'Finished Watching')">Finished Watching</button>
+                </div>
+            </div>
+
+            <div id="Future Watching" class ="tabcontent">
+                <div class="cardcontainer" id="cardcontainerID">
+                    <div class="card" id="card0" style="opacity: 1;">
+                        <div class="imagebox">
+                            <img src="https://image.tmdb.org/t/p/original/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg">
+                            <div class="buttonbox">
+                                <a href="" class="cardbutton">A</a>
+                                <a href="" class="cardbutton">B</a>
+                                <a href="" class="cardbutton">C</a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="details">
                         <h3>Avatar</h3>
-                    </div>
-                    <div class="buttonlist">
-                        <button>&#128064;</button>
-                        <button>&#8986;</button>
-                        <button>&#10003;</button>
-                        <button>&#128465;</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div id="Currently Watching" class="tabcontent">
-            <div class="moviesection">
-            <?php
-                for ($x = 0; $x < 15; $x += 1) {
-            ?>
-                <div class="card">
-                    <img class="img1" src="https://m.media-amazon.com/images/M/MV5BMTc3MDcwMTc1MV5BMl5BanBnXkFtZTcwMzk4NTU3Mg@@._V1_FMjpg_UX1000_.jpg">
-                    <img src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo-500x281.png" class="img2">
-                    <div class="content">
-                        <div class="row">
-                            <div class="details">
-                                <h3>Avatar <?=$x?></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php 
-                }
-            ?>
-            </div>
-        </div>
-
-        <div id="Finished Watching" class="tabcontent">      
-            <div class="moviesection">
-                <div class="card">
-                    <img class="img1" src="https://m.media-amazon.com/images/M/MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_QL75_UX190_CR0,2,190,281_.jpg">
-                    <div class="content">
-                        <div class="row">
-                            <div class="details">
-                                <h3>The Menu</h3>
-                            </div>
+                        <div class="hover-content">
+                        <p> LOREM IPSUM NOGWATTUS </p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    <script src="watchlist.js"></script>
+            <div id="Currently Watching" class ="tabcontent">
+                <div class="cardcontainer" id="cardcontainerID">
+                    <div class="card" id="card0" style="opacity: 1;">
+                        <div class="imagebox">
+                            <img src="https://image.tmdb.org/t/p/original/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg">
+                            <div class="buttonbox">
+                                <a href="" class="cardbutton">A</a>
+                                <a href="" class="cardbutton">B</a>
+                                <a href="" class="cardbutton">C</a>
+                            </div>
+                        </div>
+                        <h3>Avatar</h3>
+                        <div class="hover-content">
+                        <p> LOREM IPSUM NOGWATTUS </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="Finished Watching" class ="tabcontent">
+                <div class="cardcontainer" id="cardcontainerID">
+                    <div class="card" id="card0" style="opacity: 1;">
+                        <div class="imagebox">
+                            <img src="https://m.media-amazon.com/images/M/MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_QL75_UX190_CR0,2,190,281_.jpg">
+                            <div class="buttonbox">
+                                <a href="" class="cardbutton">A</a>
+                                <a href="" class="cardbutton">B</a>
+                                <a href="" class="cardbutton">C</a>
+                            </div>
+                        </div>
+                        <h3>Avatar</h3>
+                        <div class="hover-content">
+                        <p> LOREM IPSUM NOGWATTUS </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+        <script type="text/javascript" src="watchlist.js"></script>
     </body>
 </html> 
