debd0cce90f43bee6caa38879fab4eb7d01b9168
jaimie <jaimie.lauppe@student.uva.nl>
2023-01-31 14:49:41 +0100

    nav up to date

--- nav.php
+++ nav.php
@@ -8,42 +8,35 @@ require_once "/../../../conn/db.php";
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<nav id="navID">
+    <div class="nav-logo" id="logoID">
+        <p><a href="index.php">wheretowatch.com</a></p>
+    </div>
+    <div class="navbar" id="sidebar">
+        <div class="toggle-button" id="toggleButtonID">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NAV</title>
-
-    <link rel="stylesheet" href="styles/nav.css">
-</head>
-
-<body >
-    <div class="navbar" id="navdiv">
-        <nav>
-            <ul>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="about.php">ABOUT</a></li>
-                <?php
+        <ul class="computer-menu">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <?php
                 if (isset($_COOKIE['checker']) && isset($_COOKIE['login'])
                 && check_token($conn, $_COOKIE['checker'], $_COOKIE['login'])): ?>
-                <li><a href="watchlist.php">WATCHLIST</a></li>
+                <li><a href="future.php">WATCHLIST</a></li>
                 <li><a href="settings.php">SETTINGS</a></li>
                 <li><a href="account_verification/logout.php">LOGOUT</a></li>
-                <!-- <li>
-                    <div id="container">
-                        <div id="name"></div>
-                    </div>
-                </li> -->
+            <!-- <li>
+                <div id="container">
+                    <div id="name"></div>
+                </div>
+            </li> -->
 
-                <?php else: ?>
-                <li><a href="login.php">LOGIN</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
+            <?php else: ?>
+            <li><a href="login.php">LOGIN</a></li>
+            <?php endif; ?>
+        </ul>
     </div>
-</body>
-
-</html>
+</nav>
