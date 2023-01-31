6542185aee607d7b3a8b66adfba3d76e10d1e14c
danielo <daniel.oppenhuizen@student.uva.nl>
2023-01-31 13:09:50 +0100

    prep login merge

--- menuScript.js
+++ menuScript.js
@@ -4,6 +4,8 @@ document.getElementById('toggleButtonID').addEventListener('click', showMenu);
 function showMenu() {
     document.getElementById('sidebar').classList.toggle('open');
     document.getElementById('mainID').classList.toggle('open');
+    document.getElementById('footerID').classList.toggle('open');
+    document.getElementById('logoID').classList.toggle('open');
 }
 
 function showFooter() {
