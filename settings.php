a7e0e49d9566dc07098f1ea3fa29b42134b57e48
danielo <daniel.oppenhuizen@student.uva.nl>
2023-02-02 08:54:06 +0100

    fix region not working

--- settings.php
+++ settings.php
@@ -336,7 +336,7 @@ if (isset($_POST['change_password'])) {
                     <!-- Get all regions from database and put them in a dropdown -->
                     <div id = "regions" class = "all-options">
                         <?php while ($line = mysqli_fetch_array($receive)):;?>
-                        <div class = "option" onclick = "new_region('<?php echo $line[1];?>', 
+                        <div class = "option" onclick = "newRegion('<?php echo $line[1];?>', 
                         '<?php echo $_COOKIE['checker'] ?>')">
                             <input type = "radio" class = "input_php">
                             <label><?php echo $line[1];?></label>
